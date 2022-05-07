b254f5c2f378eeee519b6d59c7395ef89e57ceb8
deep-logic2000 <deeplogic2000@gmail.com>
2022-05-07 20:10:45 +0300

    delete non useable, add adaptive, fix styles & made by

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -75,90 +75,97 @@ const Converter = () => {
 
   return (
     <>
-      <div className={styles.converterWrapper}>
-        <h1 className={styles.converterTitle}>CURRENCY CONVERTER</h1>
-        <div className={styles.converterInputsWrapper}>
-          <div className={styles.converterInputWrapper}>
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": { m: 1, width: "25ch" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                id="outlined-basic"
-                label="I'm exchange"
-                variant="outlined"
-                onChange={(e) => handleChangeFromAmount(e.target.value)}
-                value={fromAmount}
-                className={styles.inputFrom}
-              />
-            </Box>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl fullWidth>
-                <InputLabel id="currency-from-label">Currency from</InputLabel>
-                <Select
-                  labelId="currency-from-label"
-                  id="currency-from-label"
-                  value={currencyFrom}
-                  label="Currency from"
-                  onChange={handleChangeFrom}
+      <section className={styles.converterWrapper}>
+        <div className={styles.mainSectionContainer}>
+          <h1 className={styles.converterTitle}>CURRENCY CONVERTER</h1>
+          <div className={styles.converterInputsWrapper}>
+            <div className={styles.converterInputWrapper}>
+              <Box
+                component="form"
+                sx={{
+                  "& > :not(style)": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="I'm exchange"
+                  variant="outlined"
+                  onChange={(e) => handleChangeFromAmount(e.target.value)}
+                  value={fromAmount}
                   className={styles.inputFrom}
-                >
-                  {currencyList &&
-                    currencyList.map((currency) => (
-                      <MenuItem key={currency} value={currency}>
-                        {currency}
-                      </MenuItem>
-                    ))}
-                </Select>
-              </FormControl>
-            </Box>
-          </div>
-          <Arrows className={styles.arrows}/>
-          <div className={styles.converterInputWrapper}>
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": { m: 1, width: "25ch" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                id="outlined-basic"
-                label="I'll recieve"
-                variant="outlined"
-                onChange={(e) => handleChangeToAmount(e.target.value)}
-                value={toAmount}
-                className={styles.inputFrom}
-              />
-            </Box>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl fullWidth>
-                <InputLabel id="currency-to-label">Currency to</InputLabel>
-                <Select
-                  labelId="currency-to-label"
-                  id="currency-to-label"
-                  value={currencyTo}
-                  label="Currency to"
-                  onChange={handleChangeTo}
+                />
+              </Box>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="currency-from-label">
+                    Currency from
+                  </InputLabel>
+                  <Select
+                    labelId="currency-from-label"
+                    id="currency-from-label"
+                    value={currencyFrom}
+                    label="Currency from"
+                    onChange={handleChangeFrom}
+                    className={styles.inputFrom}
+                  >
+                    {currencyList &&
+                      currencyList.map((currency) => (
+                        <MenuItem key={currency} value={currency}>
+                          {currency}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+            <div className={styles.arrowsWrapper}>
+              <Arrows className={styles.arrows} />
+            </div>
+            <div className={styles.converterInputWrapper}>
+              <Box
+                component="form"
+                sx={{
+                  "& > :not(style)": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="I'll recieve"
+                  variant="outlined"
+                  onChange={(e) => handleChangeToAmount(e.target.value)}
+                  value={toAmount}
                   className={styles.inputFrom}
-                >
-                  {currencyList &&
-                    currencyList.map((currency) => (
-                      <MenuItem key={currency} value={currency}>
-                        {currency}
-                      </MenuItem>
-                    ))}
-                </Select>
-              </FormControl>
-            </Box>
+                />
+              </Box>
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="currency-to-label">Currency to</InputLabel>
+                  <Select
+                    labelId="currency-to-label"
+                    id="currency-to-label"
+                    value={currencyTo}
+                    label="Currency to"
+                    onChange={handleChangeTo}
+                    className={styles.inputFrom}
+                  >
+                    {currencyList &&
+                      currencyList.map((currency) => (
+                        <MenuItem key={currency} value={currency}>
+                          {currency}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
           </div>
+          <p className={styles.madeBy}>Made by Nazarov Kirill</p>
         </div>
-      </div>
+      </section>
     </>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,9 @@ import {
 } from "../../store/actionCreators/converterAC";
 
 import styles from "./Header.module.scss";
-import flagUSA from '../../assets/svg/flagUSA.png';
-import flagUkraine from '../../assets/svg/flagUkraine.png';
-import flagEurope from '../../assets/svg/flagEurope.png';
+import flagUSA from "../../assets/svg/flagUSA.png";
+import flagUkraine from "../../assets/svg/flagUkraine.png";
+import flagEurope from "../../assets/svg/flagEurope.png";
 
 const Header = () => {
   const rateUSDUAH = useSelector(({ converter }) => converter.rates.rateUSDUAH);
@@ -24,19 +24,45 @@ const Header = () => {
 
   return (
     <>
-      <div className={styles.headerWrapper}>
-        <h1 className={styles.headerTitle}>CURRENCIES RATES</h1>
-        <div>
-          <div className={styles.rateWrapper}>
-            <h3>Rate USD <img src={flagUSA} alt='flagUSA' className={styles.flag}/> to UAH <img src={flagUkraine} alt='flagUkraine' className={styles.flag}/>:</h3>
-            <p className={styles.rateText}>{rateUSDUAH}</p>
-          </div>
-          <div className={styles.rateWrapper}>
-            <h3>Rate EUR <img src={flagEurope} alt='flagEurope' className={styles.flag}/> to UAH <img src={flagUkraine} alt='flagUkraine' className={styles.flag}/>:</h3>
-            <p className={styles.rateText}>{rateEURUAH}</p>
+      <header className={styles.headerWrapper}>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.headerTitle}>CURRENCIES RATES</h1>
+          <div>
+            <div className={styles.rateWrapper}>
+              <h3>
+                Rate USD{" "}
+                <img src={flagUSA} alt="flagUSA" className={styles.flag} /> to
+                UAH{" "}
+                <img
+                  src={flagUkraine}
+                  alt="flagUkraine"
+                  className={styles.flag}
+                />
+                :
+              </h3>
+              <p className={styles.rateText}>{rateUSDUAH}</p>
+            </div>
+            <div className={styles.rateWrapper}>
+              <h3>
+                Rate EUR{" "}
+                <img
+                  src={flagEurope}
+                  alt="flagEurope"
+                  className={styles.flag}
+                />{" "}
+                to UAH{" "}
+                <img
+                  src={flagUkraine}
+                  alt="flagUkraine"
+                  className={styles.flag}
+                />
+                :
+              </h3>
+              <p className={styles.rateText}>{rateEURUAH}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
     </>
   );
 };
